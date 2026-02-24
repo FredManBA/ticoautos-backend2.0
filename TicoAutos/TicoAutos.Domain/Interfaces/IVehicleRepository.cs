ee824577ff2b0b88e74c2d61861e0a96c0787086
alvi014 <avictorza06@gmail.com>
2026-02-24 15:42:29 -0600

    feat: core domain entities and repository interfaces

---
+++ TicoAutos/TicoAutos.Domain/Interfaces/IVehicleRepository.cs
@@ -0,0 +1,12 @@
+﻿using TicoAutos.Domain.Entities;
+
+namespace TicoAutos.Domain.Interfaces;
+
+public interface IVehicleRepository
+{
+    Task<IEnumerable<Vehicle>> GetAllAsync();
+    Task<Vehicle?> GetByIdAsync(int id);
+    Task AddAsync(Vehicle vehicle);
+    void Update(Vehicle vehicle);
+    void Delete(Vehicle vehicle);
+}
